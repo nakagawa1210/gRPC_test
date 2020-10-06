b5fe743e40c8f3eed40db96e2902f5f51e994947
nakagawa <yusuke2020@s.okayama-u.ac.jp>
2020-10-06 15:08:32 +0900

    Update ruby_grpc

--- ruby_grpc/ruby/recv_client.rb
+++ ruby_grpc/ruby/recv_client.rb
@@ -1,43 +1,19 @@
-#!/usr/bin/env ruby
-# coding: utf-8
-
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Sample app that connects to a Greeter service.
-#
-# Usage: $ path/to/greeter_client.rb
-
 this_dir = File.expand_path(File.dirname(__FILE__))
 lib_dir = File.join(this_dir, 'lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
-require 'msg_services_pb'
+require 'msg_bro_services_pb'
 
 def main
-  user = ARGV.size > 0 ?  ARGV[0] : 'world'
+  count = ARGV.size > 0 ?  ARGV[0].to_i : 10
   hostname = ARGV.size > 1 ?  ARGV[1] : 'localhost:50051'
   stub = Msg::Frame::Stub.new(hostname, :this_channel_is_insecure)
 
-  length = 4
-  
-  command = 7
-
-  dest = 10
-
-  msgid = 1000
+  length = 1
+  command = 2
+  dest = 3
+  msgid = 4
 
   
   response = stub.recv_id(Msg::IdData.new(length: length,
@@ -50,13 +26,22 @@ def main
                                           command: command,
                                           dest: dest,
                                           msgid: msgid))
-      puts "#{senddata}\n"
-      break if senddata.command == 10
+      
+      response = stub.check_msg(Msg::IdData.new(length: length,
+                                          command: command,
+                                          dest: dest,
+                                          msgid: msgid))
+      #puts "#{senddata}\n"
+      break if senddata.command == count
         
     rescue GRPC::BadStatus => e
       abort "ERROR: #{e.message}"
     end
   end
+    response = stub.time_result(Msg::IdData.new(length: length,
+                                          command: command,
+                                          dest: dest,
+                                          msgid: msgid))
 end
 
 main
--- ruby_grpc/ruby/server.rb
+++ ruby_grpc/ruby/server.rb
@@ -1,40 +1,26 @@
-#!/usr/bin/env ruby
-
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Sample gRPC server that implements the Greeter::Helloworld service.
-#
-# Usage: $ path/to/greeter_server.rb
-
 this_dir = File.expand_path(File.dirname(__FILE__))
 lib_dir = File.join(this_dir, 'lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
-require 'msg_services_pb'
+require 'msg_bro_services_pb'
 
 $array = []
 $ID = []
+$start_time = []
+$stop_time = []
 
-# GreeterServer is simple server that implements the Helloworld Greeter server.
 class MsgServer < Msg::Frame::Service
-  # say_hello implements the SayHello rpc method.
   def send_msg(iddata, _unused_call)
-    senddata = $array.shift
-    puts "#{$array}\n"
 
+    loop do
+      begin
+        break if $array.length != 0
+        sleep(0.00001)
+      end
+    end
+    
+    senddata = $array.shift
     
     Msg::SendData.new(length: senddata.length,
                       command: senddata.command,
@@ -42,10 +28,25 @@ class MsgServer < Msg::Frame::Service
                       msgid: iddata.msgid,
                       message: senddata.message)
   end
+
+  def check_msg(iddata, _unused_call)
+
+    # timer_stop
+    $stop_time.push Time.now
+    
+    Msg::Response.new(length: iddata.length,
+                      command: iddata.command,
+                      dest: iddata.dest,
+                      msgid: 0,
+                      rescode: iddata.length)
+  end
   
   def recv_msg(recvdata, _unused_call)
+
+    # timer_start
+    $start_time.push Time.now
+    
     $array.push recvdata
-    puts "#{$array}\n"
     
     Msg::Response.new(length: recvdata.length,
                       command: recvdata.command,
@@ -63,17 +64,26 @@ class MsgServer < Msg::Frame::Service
                       msgid: 0,
                       rescode: $array.length)
   end
+
+  def time_result(iddata, _unused_call)
+    n = 0
+    (1...($start_time.length + 1)).each {|start|
+      puts "#{n+=1} #{$stop_time.shift - $start_time.shift}"
+    }
+    
+    Msg::Response.new(length: iddata.length,
+                      command: iddata.command,
+                      dest: iddata.dest,
+                      msgid: 0,
+                      rescode: iddata.length)
+  end
 end
 
-# main starts an RpcServer that receives requests to GreeterServer at the sample
-# server port.
 def main
   s = GRPC::RpcServer.new
   s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
   s.handle(MsgServer)
-  # Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to 
-  #   gracefully shutdown.
-  # User could also choose to run server via call to run_till_terminated
+
   s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
 end
 
